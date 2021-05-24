1b64d09c7f19198ef23259acd758b8b513be072a
MrPos3id0n <54548481+MrPos3id0n@users.noreply.github.com>
2021-05-24 17:36:22 +0700

    Delete solve.py

--- HCMUS-CTF/2021/WeirdProtocol/solve.py
+++
@@ -1,5 +0,0 @@
-key = "hello"
-cipher = [32, 38, 33, 57, 60, 69, 38, 56, 42, 20, 
-          6, 10, 24, 51, 28, 7, 58, 27, 9, 6, 
-          26, 1, 51, 4, 10, 0, 0, 20, 8, 18, 0]
-print("".join([chr(cipher[i] ^ ord(key[i % len(key)])) for i in range(len(cipher))]))
