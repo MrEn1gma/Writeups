d043ee1e0c17c9a39d61c8d17fe952b188d628de
Hai Nguyen <54548481+MrEn1gma@users.noreply.github.com>
2025-08-06 01:10:06 +0700

    Update decrypt.py

--- CrackmeOne/Ransomware/decrypt.py
+++ CrackmeOne/Ransomware/decrypt.py
@@ -7,12 +7,13 @@ sha256_init.update(b"hackingisnotacrime")
 aes_key = sha256_init.digest()
 
 # AES Cipher
-aes_cipher = open("Received data from server\hacker", "rb").read()
+aes_cipher = open("IOC\\hacker", "rb").read()
 
 # Decrypt
 aes_init = AES.new(aes_key, AES.MODE_ECB)
 out = aes_init.decrypt(aes_cipher)
 
 # write output to file
-open("Received data from server\libgen.dll", "wb").write(out)
+open("libgen.dll", "wb").write(out)
+
 print("OK !!!")
