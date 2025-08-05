c90a607ccafa44b7fd03dc3aad23172799114603
Hai Nguyen <54548481+MrEn1gma@users.noreply.github.com>
2025-08-06 01:03:38 +0700

    Add files via upload

---
+++ CrackmeOne/Ransomware/decrypt.py
@@ -0,0 +1,18 @@
+from Cryptodome.Cipher import AES
+import hashlib
+
+# AES Key
+sha256_init = hashlib.sha256()
+sha256_init.update(b"hackingisnotacrime")
+aes_key = sha256_init.digest()
+
+# AES Cipher
+aes_cipher = open("Received data from server\hacker", "rb").read()
+
+# Decrypt
+aes_init = AES.new(aes_key, AES.MODE_ECB)
+out = aes_init.decrypt(aes_cipher)
+
+# write output to file
+open("Received data from server\libgen.dll", "wb").write(out)
+print("OK !!!")
---
+++ CrackmeOne/Ransomware/solve.py
@@ -0,0 +1,8 @@
+from Cryptodome.Cipher import ARC4
+
+arc4_key = b"r4ns0mw@rE_c4n_d357r0y_f1l3s_n0w"
+arc4_cipher = open("Received data from server\\user.html.enc", "rb").read()
+arc4_init = ARC4.new(arc4_key)
+out = arc4_init.decrypt(arc4_cipher)
+open("Received data from server\\user.html", "wb").write(out)
+print("OK !!!")
